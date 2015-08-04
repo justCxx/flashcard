a54452d15b97071d244ffaf667922d8edb8ae803
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-04 23:47:09 +0300

    Add solution of Exercise-02. Part 2

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,27 +8,42 @@ class CardsController < ApplicationController
   end
 
   def new
+    @card = Card.new
+    @card.review_date = Date.today + 3.days
   end
 
   def edit
+    @card = Card.find(params[:id])
   end
 
   def create
     @card = Card.new(card_params)
-    @card.review_date = DateTime.now.to_date
-    @card.save
-    redirect_to Card
+    if @card.save
+      redirect_to cards_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to cards_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
   end
 
   private
 
-    def card_params
-      params.require(:card).permit(:original_text, :translated_text)
-    end
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :words_different
+
+  def words_different
+    if original_text.mb_chars.downcase.to_s == translated_text.mb_chars.downcase.to_s
+      errors.add(:original_text, "can't equal translated text")
+    end
+  end
 end
