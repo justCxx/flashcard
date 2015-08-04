da771a985c6f18adba48a932f74ecff8a55876fe
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-04 18:38:40 +0300

    Add solution of Exercise-02. Part 1

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,4 +6,29 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.review_date = DateTime.now.to_date
+    @card.save
+    redirect_to Card
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text)
+    end
 end
