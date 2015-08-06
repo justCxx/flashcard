a6be10bf752623054bf5f90f7b9d34869936dac6
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-06 18:29:08 +0300

    Add solution of Exercise-05

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+    show_card_for_review
   end
+
+  def review_card
+    @card = Card.find(review_params[:id])
+    if @card.check_user_answer(params[:answer])
+      flash[:succes] = "Правильно!"
+    else
+      flash[:danger] = "Неправильно!"
+    end
+    redirect_to root_url
+  end
+
+  protected
+
+  def show_card_for_review
+    @card = Card.cards_for_review.first
+  end
+
+  def review_params
+    params.require(:card).permit(:id)
+  end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -4,13 +4,19 @@ class Card < ActiveRecord::Base
 
   before_validation :set_review_date, if: :new_record?
 
-  protected
+  scope :cards_for_review, -> {
+    where("review_date < ?", Date.today).order("RANDOM()")
+  }
 
-  def words_different
-    if normalize(original_text) == normalize(translated_text)
-      errors.add(:original_text, "can't equal translated text")
+  def check_user_answer(answer)
+    if words_equal?(answer, original_text)
+      update_attribute(:review_date, review_date + 3)
+    else
+      false
     end
   end
+
+  protected
 
   def normalize(str)
     str.strip.mb_chars.downcase.to_s
@@ -18,5 +24,15 @@ class Card < ActiveRecord::Base
 
   def set_review_date
     self.review_date = Date.today + 3.days
+  end
+
+  def words_different
+    if words_equal?(original_text, translated_text)
+      errors.add(:original_text, "can't equal translated text")
+    end
+  end
+
+  def words_equal? (word1, word2)
+    normalize(word1) == normalize(word2)
   end
 end
