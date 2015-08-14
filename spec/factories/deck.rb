7c8004b04e115b8e02d2080a23d35505da2e7391
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-15 01:29:32 +0300

    Update tests

--- spec/factories/deck.rb
+++ spec/factories/deck.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :deck do
     title "MyDeck"
-    current true
     user
   end
 end
--- spec/features/deck_spec.rb
+++ spec/features/deck_spec.rb
@@ -2,17 +2,39 @@ require "rails_helper"
 require "support/login_helper"
 
 describe "Decks" do
-  let!(:user) { FactoryGirl.create(:user, email: "f@b.ru") }
-  let!(:user_with_decks) do
-    FactoryGirl.create(:user_with_decks, email: "b@f.com", decks_count: 10)
+  let!(:deck) do
+    FactoryGirl.create(:deck)
   end
 
   before(:each) do
-    login("f@b.ru", "foobar")
+    login("foo@bar.com", "foobar")
   end
 
-  it "add new deck" do
+  it "titled deck added" do
+    deck_count = deck.user.decks.count
     visit new_deck_path
-    expect(current_path).to eq(new_deck_path)
+    fill_in("deck_title", with: "MyDeck")
+    click_button "Create Deck"
+    expect(deck.user.decks.count).to be (deck_count + 1)
+  end
+
+  it "untitled deck is no added" do
+    deck_count = deck.user.decks.count
+    visit new_deck_path
+    fill_in("deck_title", with: "")
+    click_button "Create Deck"
+    expect(deck.user.decks.count).to be (deck_count)
+  end
+
+  it "show cards in deck" do
+    visit deck_path(deck)
+    expect(current_path).to eq deck_cards_path(deck)
+  end
+
+  it "update deck title" do
+    visit edit_deck_path(deck)
+    fill_in("deck_title", with: "TestDeck")
+    click_button "Update Deck"
+    expect(deck.user.decks.first.title).to eq "TestDeck"
   end
 end
