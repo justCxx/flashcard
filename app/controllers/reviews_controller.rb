014d773be56dd9c4b30c7ddbc8d6b1f7898a2435
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-20 10:20:03 +0300

    Add Exercise-14 solution

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,21 @@ class ReviewsController < ApplicationController
 
   def create
     @card = current_user.cards.find(review_params[:card_id])
-    if @card.review(review_params[:answer])
+    review = @card.review(review_params[:answer])
+
+    case review[:typos]
+    when 0
       flash[:success] = "Right! Next review: #{@card.review_date.localtime}"
+    when 1..Card::MAX_LEVENSHTEIN_DISTANCE
+      flash[:success] = "Almost correct. Original: #{@card.original_text}, " \
+                        "translated: #{@card.translated_text}. " \
+                        "Your answer: #{review[:user_answer]}. " \
+                        "Typos: #{review[:typos]}. " \
+                        "Next review: #{@card.review_date.localtime}"
     else
       flash[:danger] = "Wrong! Next review: #{@card.review_date.localtime}"
     end
-    redirect_to root_url
+    redirect_to new_review_path
   end
 
   protected
